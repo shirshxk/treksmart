bd9901d0f6269bf6f14e4bea9af8fce7401cbcd0
shirshxk <sirshak99@gmail.com>
2025-02-14 14:16:49 +0545

    resolved

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,6 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
 session_start();
 >>>>>>> ea3dcc8 (update)
 // Start of admin dashboard page: Fetch data and set active section.
@@ -9,9 +7,6 @@ $activeSection = $_GET['section'] ?? 'manageUsers';
 
 // Check if an admin is logged in; if not, redirect to login.
 if (!isset($_SESSION['user_id']) || $_SESSION['admin_check'] != 1) {
-<<<<<<< HEAD
-    header("Location: admin_login.php");
-=======
     header("Location: ../auth/login.php");
 >>>>>>> ea3dcc8 (update)
     exit;
